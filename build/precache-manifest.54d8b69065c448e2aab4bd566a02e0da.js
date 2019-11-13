17578fd2d4e25ffe9465ae9640efbdf7b2324bce
wasinaphat lilawatthananan <wasinapl@scg.com>
2019-11-13 10:40:25 +0700

    first commit

---
+++ build/precache-manifest.54d8b69065c448e2aab4bd566a02e0da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8798f00169122a7439bde5607c3e8d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e361b36be77532084d2c",
+    "url": "/static/js/2.0ec633fd.chunk.js"
+  },
+  {
+    "revision": "6661cdec95ae99cde1ca",
+    "url": "/static/js/main.14fd3289.chunk.js"
+  },
+  {
+    "revision": "e7a3c116ba04f3f779fd",
+    "url": "/static/js/runtime-main.2915aa69.js"
+  },
+  {
+    "revision": "3868e2cbf6f0fa677ba86e92e66379ca",
+    "url": "/static/media/moutain.3868e2cb.jpg"
+  }
+]);
